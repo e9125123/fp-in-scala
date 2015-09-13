f6d944147d1ca4ddeaca40e1973f4914c516a29b
alex <alex@mclachlan.at>
2015-09-14 01:06:46 +0200

    cleanup

--- src/test/scala/Chapter2Spec.scala
+++ src/test/scala/Chapter2Spec.scala
@@ -1,11 +1,11 @@
-import org.scalatest.{Matchers, FunSpec}
+import org.scalatest._
 import Chapter2._
 /**
  * Tests for Chapter2 exercises
  */
-class Chapter2Spec extends FunSpec with Matchers  {
+class Chapter2Spec extends FlatSpec with Matchers  {
 
-  it("Exercise 2.1: fib sequence must be correct") {
+  "Exercise 2.1" should "define fib function" in {
     fib(0) should be (0)
     fib(1) should be (1)
     fib(2) should be (1)
@@ -15,7 +15,7 @@ class Chapter2Spec extends FunSpec with Matchers  {
     fib(6) should be (8)
   }
 
-  it("Exercise 2.2: isSorted must be correct") {
+  "Exercise 2.2" should "define isSorted function" in {
     isSorted(Array(1,2,3), (i: Int, j: Int) => j > i) should be (true)
     isSorted(Array(4,2,3), (i: Int, j: Int) => j > i) should be (false)
     isSorted(Array(1,1,3), (i: Int, j: Int) => j >= i) should be (true)
