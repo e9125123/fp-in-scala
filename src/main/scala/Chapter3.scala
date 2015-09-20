c1206c5ae323d9189f9f81df7674ffb14c2e1446
alex <alex@mclachlan.at>
2015-09-20 23:57:56 +0200

    chapter 3 first twp exercises

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -3,4 +3,35 @@
  */
 object Chapter3 {
 
+  sealed trait List[+A]
+  case object Nil extends List[Nothing]
+  case class Cons[A](head: A, tail: List[A]) extends List[A]
+
+  object List {
+    def sum(ints: List[Int]): Int = ints match {
+      case Nil => 0
+      case Cons(x, xs) => x + sum(xs)
+    }
+
+    def product(ds: List[Double]): Double = ds match {
+      case Nil => 1.0
+      case Cons(0.0, _) => 0.0
+      case Cons(h, t) => h * product(t)
+    }
+
+    def apply[A](as: A*): List[A] = {
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+    }
+
+    /**
+     * Implement the function tail for removing the first element of a List . Note that the
+     * function takes constant time. What are different choices you could make in your
+     * implementation if the List is Nil ? We’ll return to this question in the next chapter.
+     */
+    def tail[A](l: List[A]): List[A] = l match {
+      case Nil => Nil
+      case Cons(h, t) => t
+    }
+  }
 }
--- src/test/scala/Chapter3Spec.scala
+++ src/test/scala/Chapter3Spec.scala
@@ -1,4 +1,4 @@
-import Chapter2._
+import Chapter3._
 import org.scalatest._
 
 /**
@@ -6,6 +6,30 @@ import org.scalatest._
  */
 class Chapter3Spec extends  UnitSpec {
 
-  "Exercise 3.1" should "whatever" in {
+  "List example" should "show the usage of List" in {
+    List.sum(Cons(2, Cons(3, Nil))) should be (5)
+    List.product(Cons(2.0, Cons(2.0, Nil))) should be (4.0)
+
+    List(2, 3) should be (Cons(2, Cons(3, Nil)))
+
+    List.sum(List(2, 3)) should be (5)
+    List.product(List(2.0, 2.0)) should be (4.0)
+  }
+
+  "Exercise 3.1" should "show understanding of match" in {
+    val x = List(1,2,3,4,5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + List.sum(t)
+      case _ => 101
+    }
+
+    x should be (3)
+  }
+
+  "Exercise 3.2" should "implement tail" in {
+    List.tail(Nil) should be (Nil)
+    List.tail(List(1,2,3,4,5)) should be (List(2,3,4,5))
   }
 }
