2f1cd769c5270655075bdba1dc528ef3ec0a3491
alex <alex@mclachlan.at>
2015-09-27 23:27:01 +0200

    added Option[A] code

--- src/main/scala/Chapter4.scala
+++ src/main/scala/Chapter4.scala
@@ -1,6 +1,10 @@
 /**
- * Code to go with chapter 4 of 'Functional Programmming in Scala'
+ * Code to go with chapter 4 of 'Functional Programming in Scala'
  */
 object Chapter4 {
+
+  sealed trait Option[+A]
+  case class Some[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
 
 }
