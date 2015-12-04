2bdbaea4097cbb954b48262a52f393dc42250dad
alex <alex@mclachlan.at>
2015-12-04 21:36:59 +0100

    Exercise 5.2

--- src/main/scala/Chapter5.scala
+++ src/main/scala/Chapter5.scala
@@ -2,6 +2,7 @@
  * Code to go with chapter 5 of 'Functional Programming in Scala'
  */
 
+import scala.collection.immutable.Stream.cons
 import scala.collection.immutable._
 
 object Chapter5 {
@@ -9,12 +10,36 @@ object Chapter5 {
   sealed trait Stream[+A] {
     def toList: List[A] = this match {
       case Empty => List()
-      case Cons(h, t) => t().toList.::(h())
+      case Cons(h, t) => h() :: t().toList
     }
 
     def headOption: Option[A] = this match {
       case Empty => None
       case Cons(h, t) => Some(h())
+    }
+
+    def take(i: Int): Stream[A] = {
+      if ( i<= 0) Empty
+      else {
+        this match {
+          case Empty => Empty
+          case Cons(h, t) => {
+            Cons(h, () => t().take(i - 1))
+          }
+        }
+      }
+    }
+
+    def drop(i: Int): Stream[A] = {
+      if ( i<= 0) this
+      else {
+        this match {
+          case Empty => Empty
+          case Cons(h, t) => {
+            t().drop(i-1)
+          }
+        }
+      }
     }
   }
   case object Empty extends Stream[Nothing]
--- src/test/scala/Chapter5Spec.scala
+++ src/test/scala/Chapter5Spec.scala
@@ -14,4 +14,18 @@ class Chapter5Spec extends UnitSpec {
     val s = Stream(1, 2, 3)
     s.toList  should be(List(1, 2, 3))
   }
+
+  /**
+    * Write the function take(n) for returning the first n elements of a Stream , and
+    * drop(n) for skipping the first n elements of a Stream .
+    */
+  "Exercise 5.2" should "implement take and drop" in {
+    val s = Stream(1, 2, 3, 4)
+    s.take(2).toList  should be(List(1, 2))
+    s.drop(2).toList  should be(List(3, 4))
+
+    // TODO: should also work for stream equal
+//    s.take(2) should be(Stream(1, 2))
+//    s.drop(2) should be(Stream(3, 4))
+  }
 }
